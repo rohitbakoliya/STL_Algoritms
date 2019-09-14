e2726cc8b81c53057cb01cab5fa02ab70e837cca
ROHIT BAKOLIYA <46809038+rohitbakoliya@users.noreply.github.com>
2019-09-14 17:06:20 +0530

    Update algorithms-part2.cpp

--- algorithms-part2.cpp
+++ algorithms-part2.cpp
@@ -24,4 +24,21 @@ int main()
 		cout<<endl;
 	}while(prev_permutation(all(vect1)));
 	
+	
+	vector< int > v{ 1, 2,3 ,4 ,5 , 6,7 , 8};
+	long long sum = accumulate(all(v), (long long)0);
+	double product = accumulate(all(v), double(1), multiplies< double >());
+	
+	
+	vector< int > v1;
+	vector< int > v2;
+	for(int i = 0; i < 3; i++) {
+	v1.push_back(10-i);
+	v2.push_back(i+1);
+	}
+	int r = inner_product(all(v1), v2.begin(), 0);// r holds (v1[0]v2[0] + v1[1]v2[1] + v1[2]v2[2]), or (101+92+83), which is 52.
+	int r1 = inner_product(all(v), v.rbegin(), 0); //V[0]*V[N-1] + V[1]+V[N-2] + ... + V[N-1]*V[0] where N is the number of elements in 'v'.
+
+
+
 }
