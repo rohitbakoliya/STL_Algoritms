bc55ae8f9c5342a65123d4ad5324005206195d5c
ROHIT BAKOLIYA <46809038+rohitbakoliya@users.noreply.github.com>
2019-10-08 17:09:22 +0530

    Update algorithm-part-3.cpp

--- algorithm-part-3.cpp
+++ algorithm-part-3.cpp
@@ -1,3 +1,5 @@
+//https://www.youtube.com/watch?v=58BrFvjNhWY
+//for lambda expressions
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -7,4 +9,22 @@ int main()
     vector<int> v2(16 , 5);
     copy_if(v.begin() , v.end() , v2.begin() , [](int x){return x%2==0;});
     for(auto i : v2) cout<<i<<endl;
+    
+    // lambda functions
+    [](){}; //it is valid state ment in c++ only gives warning 
+    [](){ cout<<"hello world"<<endl }; //nothing will happen ....cause function is not called
+    [](){ cout<<"hello world"<<endl }(); // hello world printed...
+    
+    int ry =100;
+    [ry](){ cout<<ry<<endl;}(); // passing variable inside capchalist 
+//   we can pass local variable inside anonymous functions (i.e lambda functions ) by value and refereance (i.e &ry ) 
+    
+    
+    int x = []()-> int {return 100; }(); // for return type
+    int x = []( int base )->int {return base+100; }(200); ///for arguments
+   
+    //function is in #include<functional>
+    function<int(int)> functionPointer = [](int base)-> int{ return base + 100 ; };
+    int returnValue = functionPointer(50);
+    cout<<returnValue<<endl;
 }
